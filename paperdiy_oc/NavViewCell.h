114d506c65d4ad0baf32395f73d465458f77bfce
RexFun <oleFun@icloud.com>
2016-02-15 18:01:38 +0800

    init commit

---
+++ paperdiy_oc/NavViewCell.h
@@ -0,0 +1,15 @@
+//
+//  NavViewCell.h
+//  paperdiy_oc
+//
+//  Created by mac373 on 16/2/15.
+//  Copyright © 2016年 rexfun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavViewCell : UITableViewCell
+
+@property (nonatomic, strong) UILabel *sectionName;
+
+@end
---
+++ paperdiy_oc/NavViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavViewController.h
+//  paperdiy_oc
+//
+//  Created by mac373 on 16/2/15.
+//  Copyright © 2016年 rexfun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavViewController : UITableViewController
+
+@end
