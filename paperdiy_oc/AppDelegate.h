6b18b22c09cb58dd92ba1a77882bdd727b836bce
RexFun <oleFun@icloud.com>
2016-02-15 16:46:38 +0800

    Initial Commit

---
+++ paperdiy_oc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  paperdiy_oc
+//
+//  Created by mac373 on 16/2/15.
+//  Copyright © 2016年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
